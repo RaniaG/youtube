3b063dbbd03ecce474de1c3299c1a5fbe638631e
RaniaG <raniagml12@gmail.com>
2022-02-01 18:32:04 +0200

    create functions and urls for interfacing with youtube api

---
+++ src/api/api.ts
@@ -0,0 +1,55 @@
+import axios from "axios";
+import { from, map, Observable, of } from "rxjs";
+import { urls } from "../constants/urls";
+import { ChannelStatistics } from "../models/channel-statistics";
+import { PlaylistDetails } from "../models/playlist-details";
+import { SearchResult } from "../models/search-result";
+import { VideoDetails } from "../models/video-details";
+
+export const API_KEY = "AIzaSyCqNkUgk4PasiBNxWYpl5WiuhOAXo9r8nY";
+
+export function getChannelStatistics(
+  channelId: string
+): Observable<ChannelStatistics> {
+  return from(
+    axios.get(`${urls.channel}?part=statistics&id=${channelId}&key=${API_KEY}`)
+  ).pipe(map((res) => res.data?.items[0].statistics));
+}
+
+export function getVideoDetails(videoId: string): Observable<VideoDetails> {
+  return from(
+    axios.get(
+      `${urls.video}?part=statistics,contentDetails&id=${videoId}&key=${API_KEY}`
+    )
+  ).pipe(
+    map((res) => ({
+      statistics: res.data?.items[0]?.statistics,
+      duration: res.data?.items[0]?.contentDetails?.duration,
+    }))
+  );
+}
+
+export function getPlaylistDetails(
+  playlistId: string
+): Observable<PlaylistDetails> {
+  return from(
+    axios.get(
+      `${urls.playlist}?part=contentDetails&id=${playlistId}&key=${API_KEY}`
+    )
+  ).pipe(
+    map((res) => ({
+      itemCount: res.data?.items[0]?.contentDetails?.itemCount,
+    }))
+  );
+}
+
+export function getSearchResults(
+  queryString: string
+): Observable<SearchResult> {
+  if (!queryString) return of();
+  return from(
+    axios.get(
+      `${urls.search}?part=snippet,id&${queryString}&maxResults=25&key=${API_KEY}`
+    )
+  ).pipe(map((res) => res.data));
+}
---
+++ src/constants/urls.ts
@@ -0,0 +1,7 @@
+export const urls = {
+  channel_url: "",
+  channel: "https://www.googleapis.com/youtube/v3/channels",
+  search: "https://www.googleapis.com/youtube/v3/search",
+  video: "https://www.googleapis.com/youtube/v3/videos",
+  playlist: "https://www.googleapis.com/youtube/v3/playlists",
+};
