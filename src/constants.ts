1a507ed70f9fb592904fa20046c982be23b3120a
RaniaG <raniagml12@gmail.com>
2022-02-02 02:03:55 +0200

    add missing urls

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,8 @@
 export const urls = {
   channel_url: "",
+  video_redirect_url: `https://www.youtube.com/watch?v=`,
+  channel_redirect_url: `https://www.youtube.com/channel/`,
+  playlist_redirect_url: `https://www.youtube.com/playlist?list=`,
   channel: "https://www.googleapis.com/youtube/v3/channels",
   search: "https://www.googleapis.com/youtube/v3/search",
   video: "https://www.googleapis.com/youtube/v3/videos",
