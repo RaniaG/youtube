759f406e4c4b3d789ab413fc80cf83eca141210c
RaniaG <raniagml12@gmail.com>
2022-02-02 02:03:41 +0200

    add loading component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Loading } from "./components/loading/loading";
 import { Header } from "./containers/header/header";
 import { Search } from "./pages/search/search";
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <Header />
+        <Loading />
         <Routes>
           <Route path="/search" element={<Search />}></Route>
         </Routes>
